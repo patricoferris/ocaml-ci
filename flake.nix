f5da65481fdbaf4f4c0aa7a5249aab5c79ed435d
Patrick Ferris <patrick@sirref.org>
2026-04-14 14:36:09 +0100

    Pin-depends for nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  inputs.opam-nix = {
+    url = "github:tweag/opam-nix";
+    inputs.nixpkgs.follows = "nixpkgs";
+    inputs.flake-utils.follows = "flake-utils";
+    inputs.opam-repository.follows = "opam-repository";
+  };
+  inputs.opam-repository = {
+    url = "github:ocaml/opam-repository";
+    flake = false;
+  };
+
+  outputs = { self, nixpkgs, opam-nix, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      legacyPackages =
+        let 
+          inherit (opam-nix.lib.${system}) buildOpamProject';
+          scope = buildOpamProject' { pinDepends = true; } ./. { ocaml-base-compiler = "*"; };
+        in
+        scope;
+
+      packages.default = self.legacyPackages.${system}.ocaml-ci-service;
+      });
+}
